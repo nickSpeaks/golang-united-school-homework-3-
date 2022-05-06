06910f7ef431a1488d06f711cba8de39de9ac476
Mac User <macuser@MacBook-Air-Mac.local>
2022-05-06 17:38:43 +0400

    arrays ready

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,10 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
+	result = 0
+	for _, n := range input {
+		result += n
+	}
+	result /= 15
 	return
 }
