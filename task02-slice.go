418e8ac61a99e28a657b31b0e8e73d596d5b700b
nickSpeaks <andrejeu@gmail.com>
2022-05-19 01:18:54 +0300

    slices complete

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,9 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
+	result := []int64{}
+	for i := len(input) - 1; i >= 0; i-- {
+		result = append(result, input[i])
+	}
 	return
 }
