83697b2de6e8f0a7837b329ef34a30e58a817a45
nickSpeaks <andrejeu@gmail.com>
2022-05-19 12:38:46 +0300

    correct maps

--- task03-map.go
+++ task03-map.go
@@ -1,20 +1,24 @@
 package homework
 
-func sortMapValues(input map[int]string) (result []string) {
-		// make array to store keys
-		keys := make([]int, 0, len(input))
+import "sort"
 
-		for k := range input {
-			keys = append(keys, k)
-		}
-		// sort keys in array
-		sort.Ints(keys)
-	
-		// make arrray to store values
-		result := []string{}
-	
-		// store values in array
-		for _, value := range keys {
-			result = append(result, input[value])
-	return
+func sortMapValues(input map[int]string) (result []string) {
+	// make array to store keys
+	keys := make([]int, 0, len(input))
+
+	// write keys into array
+	for k := range input {
+		keys = append(keys, k)
+	}
+	// sort keys in array
+	sort.Ints(keys)
+
+	// make arrray to store values
+	results := []string{}
+
+	// store values in array
+	for _, value := range keys {
+		results = append(results, input[value])
+	}
+	return results
 }
