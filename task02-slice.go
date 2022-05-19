66e5bc683de2f091f4e13698ef9a21ee5d1a441e
nickSpeaks <andrejeu@gmail.com>
2022-05-19 11:24:58 +0300

    add mod & correct reverse func in slices

--- task02-slice.go
+++ task02-slice.go
@@ -1,9 +1,9 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	result := []int64{}
+	res := make([]int64, 0, len(input))
 	for i := len(input) - 1; i >= 0; i-- {
-		result = append(result, input[i])
+		res = append(res, input[i])
 	}
-	return
+	return res
 }
